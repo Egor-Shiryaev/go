fae3e733389b1f52abebaaf16cfb62593f9e77e6
Egor-Shiryaev <euro24rus@gmail.com>
2022-12-28 00:48:20 +0700

    first commit

--- app.go
+++ app.go
@@ -1,67 +1,153 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	strSlice := strings.Split(line, " ")
 
-	type Point struct {
-		X int
-		Y int
+	if len(strSlice) != 3 {
+		fmt.Println(errors.New("некорректные входные данные"))
+		return
 	}
 
-	pointsMap := map[string]Point{} // объявили и проинициализировали
+	num1 := strSlice[0]
+	num2 := strSlice[2]
+	operator := strSlice[1]
 
-	pointsMap["a"] = Point{
-		// добавление элементов в map
-		X: 1,
-		Y: 2,
-	}
-	pointsMap["b"] = Point{
-		X: 3,
-		Y: 4,
+	checkOperator, _ := regexp.MatchString(`[^-+*/]`, operator)
+	if checkOperator {
+		fmt.Println(errors.New("калькутор умеет выполнть только операции: + - * /")) // true
+		return
 	}
 
-	fmt.Println(pointsMap)        // map[a:{1 2} b:{3 4}]
-	fmt.Println(pointsMap["a"])   // {1 2}
-	fmt.Println(pointsMap["b"].X) // 3
+	if !(checkArab(num1) && checkArab(num2) || checkRim(num1) && checkRim(num2)) {
+		fmt.Println(errors.New("вычисляет одновременно только с римскими или арабскими цифрами"))
+		return
+	}
 
-	pointsMap2 := make(map[int]Point)
-	pointsMap2[1] = Point{1, 2} // ри инициализации структуры могу просто написать значения, без ключей
-	fmt.Println(pointsMap2)     // map[1:{1 2}]
-	fmt.Println(pointsMap2[1])  // {1 2}
-	// добавление элементов в map
+	if (checkArab(num1) && checkArab(num2)) && (strToNum(num1) > 10 || strToNum(num2) > 10) {
+		fmt.Println(errors.New("число должно быть меньше 10"))
+		return
+	}
 
-	//проверяем на пустоту, если да то добавляем
-
-	var pointsMap3 map[string]Point // объявлен но не инициализирован
-	if pointsMap3 == nil {
-		pointsMap3 = map[string]Point{
-			"a": {1, 2},
+	if checkRim(num1) && checkRim(num2) {
+		a := rimToArab(num1)
+		b := rimToArab(num2)
+		res := arifm(a, b, operator)
+		if res <= 0 {
+			fmt.Println(errors.New("результат работы с римскими числами должен быть больше нуля"))
+			return
+		} else {
+			fmt.Println(integerToRoman(res))
+			return
 		}
 	}
 
-	// есть ли в мапе значению по ключу
-	key := "a"
-	value, ok := pointsMap3[key]
-	if ok {
-		fmt.Printf("key=%s exist in map\n", key)
-		fmt.Println(value)
-	} else {
-		fmt.Printf("key=%s does in map\n", key)
-		fmt.Println(value)
-
+	if checkArab(num1) && checkArab(num2) {
+		fmt.Println(arifm(strToNum(num1), strToNum(num2), operator))
+		return
 	}
+}
 
-	//	 декодирование мапа в структуру. Из мапа pointsMap4, получить структуру Pointt
-
-	type Pointt struct {
-		X int
-		Y int
+func checkRim(numStr string) bool {
+	var arr = [10]string{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}
+	for _, elem := range arr {
+		if elem == numStr {
+			return true
+		}
 	}
+	return false
+}
 
-	pointsMap4 := map[string]int{
-		"x": 1,
-		"y": 2,
+func checkArab(numStr string) bool {
+	_, err := strconv.Atoi(numStr)
+	if err != nil {
+		return false
 	}
+	return true
+}
 
+// Из строки  "1" в число 1
+func strToNum(str string) int {
+	x, _ := strconv.Atoi(str)
+	return x
+}
+
+// Арифметическая операция
+func arifm(a int, b int, c string) int {
+	var res int
+	switch c {
+	case "+":
+		res = a + b
+	case "-":
+		res = a - b
+	case "*":
+		res = a * b
+	case "/":
+		res = a / b
+	}
+	return res
+}
+
+func rimToArab(numArab string) int {
+	var res int
+	switch numArab {
+	case "I":
+		res = 1
+	case "II":
+		res = 2
+	case "III":
+		res = 3
+	case "IV":
+		res = 4
+	case "V":
+		res = 5
+	case "VI":
+		res = 6
+	case "VII":
+		res = 7
+	case "VIII":
+		res = 8
+	case "IX":
+		res = 9
+	case "X":
+		res = 10
+	}
+	return res
+}
+
+func integerToRoman(number int) string {
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+	var roman strings.Builder
+	for _, conversion := range conversions {
+		for number >= conversion.value {
+			roman.WriteString(conversion.digit)
+			number -= conversion.value
+		}
+	}
+	return roman.String()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module awesomeProject1
 
 go 1.19
+
+require github.com/mitchellh/mapstructure v1.5.0
